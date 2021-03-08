a9f2a7b6c1a3fa50a8b12ef2b3ebad1e5769fe7d
jinyy2 <jy5031le@gmail.com>
2021-03-08 17:47:37 +0900

    mustache

--- src/test/java/com/jinyy2/book/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/jinyy2/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -7,6 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -31,6 +36,29 @@ public class PostsRepositoryTest {
         .content(content)
         .author("jinyy2@github.com")
         .build());
+
+    }
+
+    @Test
+    public  void BaseTimeEntity_등록(){
+        //given
+        LocalDateTime now = LocalDateTime.of(2019,6,4,0,0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>> createDate="+posts.getCreatedDate()+", modifiedDate="+posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
 
     }
 }
