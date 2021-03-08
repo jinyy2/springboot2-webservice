03d616d7fa703b40f63e59aeb7240e343b5f28e9
jinyy2 <jy5031le@gmail.com>
2021-03-08 20:55:08 +0900

    게시글 조회

--- src/main/java/com/jinyy2/book/springboot/web/IndexController.java
+++ src/main/java/com/jinyy2/book/springboot/web/IndexController.java
@@ -1,13 +1,22 @@
 package com.jinyy2.book.springboot.web;
 
+import com.jinyy2.book.springboot.domain.posts.PostsService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
+    public IndexController(PostsService postsService) {
+        this.postsService = postsService;
+    }
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("posts",postsService.findAllDesc());
         return "index";
     }
 
